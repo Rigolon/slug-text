5f33e95b47451b7ef6d24f282bc7b57ce078f841
Bruno <bruno@pontadesign.com.br>
2015-09-23 12:10:44 -0300

    Update slug.js

--- slug.js
+++ slug.js
@@ -4,13 +4,13 @@ function slug(str) {
   str = str.toLowerCase();
 
   // remove accents, swap ñ for n, etc
-  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç./_,:;";
+  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
   var to   = "aaaaaeeeeeiiiiooooouuuunc------";
   for (var i=0, l=from.length ; i<l ; i++) {
     str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
   };
 
-  str = str.replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
+  str = str.replace(/\./g, '-').replace(/[^a-z0-9 -.]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
 
   return str;
 }
