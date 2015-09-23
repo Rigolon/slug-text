e060762e4858da429a10ab5f766b9f5e0d80c2fb
Bruno <bruno@pontadesign.com.br>
2015-09-23 12:04:38 -0300

    Create slug.js
    Script em Javascript pra fazer a conversão do texto para texto limpo.

---
+++ slug.js
@@ -0,0 +1,16 @@
+function slug(str) {
+
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+  var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  };
+
+  str = str.replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
+
+  return str;
+}
