c57ce7d44341370a3c33d75a826e8e8a6656ad98
Bruno <bruno@pontadesign.com.br>
2016-02-16 16:43:48 -0200

    Update slug.php
    Código mais simples e fácil de compreender.

--- slug.php
+++ slug.php
@@ -1,23 +1,16 @@
-function slug($string)
-{
-  	setlocale(LC_ALL, 'pt_BR.UTF8');
-	$string = utf8_encode($string);
-	
-	$table = array(
-    	'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
-    	'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-    	'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
-    	'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
-    	'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
-    	'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
-    	'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
-    	'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
-  	);
+function slug($str, $replace=array(), $delimiter='-')
+{    
+    setlocale(LC_ALL, 'en_US.UTF8');
 
-  	$string = strtr($string, $table);
-	
-	$string= iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $string);
-	$string = mb_convert_encoding(utf8_encode($string), 'utf-8', 'auto' );
-	
-	return strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $string));
+    if( !empty($replace) )
+    $str = str_replace((array)$replace, ' ', $str);
+
+    $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+    $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+    $clean = strtolower(trim($clean, '-'));
+    $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+
+    $clean = strtolower(trim($clean, '-'));
+
+    return $clean;
 }
